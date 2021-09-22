5d3c25a74b1259fe045c354af45d8dac38228375
deok2kim <dukgi77@naver.com>
2021-09-22 22:26:44 +0900

    1.0 React + Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAjeJyGSuC2X3dzlGjgA1prox7fIyKoEWQ",
+	authDomain: "nwitter-e3515.firebaseapp.com",
+	projectId: "nwitter-e3515",
+	storageBucket: "nwitter-e3515.appspot.com",
+	messagingSenderId: "457259424884",
+	appId: "1:457259424884:web:fc86c4604a913c61990521"
+};
+  
+
+export default firebase.initializeApp(firebaseConfig);
