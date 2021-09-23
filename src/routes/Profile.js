dff8126b0ef5aaa1cce18bc9fa6eacc26ae76128
deok2kim <deok2kim>
2021-09-23 20:49:06 +0900

    2.6 Log Out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,19 @@
 import React from 'react';
-
+import { signOut } from '@firebase/auth';
+import { authService } from 'fbase';
+import { useHistory } from 'react-router';
 const Profile = () => {
-    return <span>Profile</span>
+    const history = useHistory();
+    const onLogOutClick = () => {
+        signOut(authService);
+        console.log("로그아웃");
+        history.push('/');
+    }
+    return (
+        <>
+            <button onClick={onLogOutClick} >Log Out</button>
+        </>
+    )
 };
 
 export default Profile;
