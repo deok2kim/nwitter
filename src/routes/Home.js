0eac5a57ef6983c8f8362edb4e17d9b3451c8d31
deok2kim <deok2kim>
2021-09-23 22:46:03 +0900

    3.1 Nweeting!

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
+import { dbService } from 'fbase';
+import { addDoc, collection } from '@firebase/firestore';
 
 const Home = () => {
     const [nweet, setNweet] = useState('');
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
+        await addDoc(collection(dbService, "nweets"), {
+            nweet,
+            createdAt: new Date(),
+        });
+        setNweet('');
     }
     const onChange = (e) => {
         setNweet(e.target.value);
+
     }
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input type="text" placeholder="What's on your mind" maxLength={120} onChange={onChange} />
+                <input type="text" placeholder="What's on your mind" maxLength={120} onChange={onChange} value={nweet} />
                 <input type="submit" value="Nweet" />
             </form>
         </div>
