51ea2cdd364fc61faed2333b56c55870a608db16
deok2kim <deok2kim>
2021-09-23 16:38:16 +0900

    1.2 Router Setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/compat/app";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyAjeJyGSuC2X3dzlGjgA1prox7fIyKoEWQ",
-	authDomain: "nwitter-e3515.firebaseapp.com",
-	projectId: "nwitter-e3515",
-	storageBucket: "nwitter-e3515.appspot.com",
-	messagingSenderId: "457259424884",
-	appId: "1:457259424884:web:fc86c4604a913c61990521"
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MASSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
 };
   
 
