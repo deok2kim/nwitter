0583f00d34d9dd7a5a5ae7b1ee6b02599d006c1c
deok2kim <dukgi77@naver.com>
2021-09-26 15:39:53 +0900

    4.2 Uploading

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { dbService } from 'fbase';
-import { addDoc, collection, onSnapshot } from '@firebase/firestore';
+import { dbService, storageService } from 'fbase';
+import { collection, onSnapshot } from '@firebase/firestore';
+import { ref, uploadString } from '@firebase/storage';
 import Nweet from 'components/Nweet';
+import { v4 as uuidv4 } from 'uuid';
 
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState('');
@@ -20,12 +22,15 @@ const Home = ({ userObj }) => {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    await addDoc(collection(dbService, 'nweets'), {
-      text: nweet,
-      createdAt: new Date(),
-      creatorId: userObj.uid,
-    });
-    setNweet('');
+    const fileRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+    // await addDoc(collection(dbService, 'nweets'), {
+    //   text: nweet,
+    //   createdAt: new Date(),
+    //   creatorId: userObj.uid,
+    // });
+    // setNweet('');
+    const response = await uploadString(fileRef, attachment, 'data_url');
+    console.log(response);
   };
   const onChange = (e) => {
     setNweet(e.target.value);
